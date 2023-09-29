77619a7661c65e41e33d29c6648bb6655492c68b
planetReebok <114269604+planetReebok@users.noreply.github.com>
2023-09-29 09:59:54 +0800

    started on JS for homework site

---
+++ homework_01b/script.js
@@ -0,0 +1,9 @@
+//console.log("Hello");
+
+
+let trigger = document.getElementById('backgroundTrigger');
+
+trigger.addEventListener('click', function () {
+    console.log("trigger");
+    console.log("background clicked")
+})
